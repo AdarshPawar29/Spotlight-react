eb7e561de7e199a2d1aa0a17b440c2d274de575b
AdarshPawar29 <Adarsh.11601516@lpu.in>
2021-10-20 15:03:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { StoplightProject } from "@stoplight/elements-dev-portal";
+import "@stoplight/elements-dev-portal/styles.min.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StoplightProject projectId="cHJqOjgzMTk3" />
     </div>
   );
 }
